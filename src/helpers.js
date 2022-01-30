2cca99274855da604c260f85e282dcb54965e99c
MaxOnGreeeeen <mas81@tpu.ru>
2022-01-30 07:35:12 +0700

    First commit
    Managed to start creating react-app

---
+++ src/helpers.js
@@ -0,0 +1,22 @@
+
+const caluclate = (value) =>{
+    const parsed = parse(value);
+
+    return eval(parsed);
+}
+
+const parse = (value) =>{
+    return value;
+}
+
+const inc = (value) =>{
+    return value;
+}
+
+const dec = (value) =>{
+    return value;
+}
+
+export default caluclate;
+
+
